126513ff58584a78eca53e963b78faacd2465482
Peter Schüllermann <peter.schuellermann@gmail.com>
2016-01-15 19:55:51 +0100

    add product initialization and validation

--- lib/product.rb
+++ lib/product.rb
@@ -1,3 +1,24 @@
-class Product
+# require 'active_support'
+require 'active_support/core_ext/array/extract_options.rb'
 
+class Product
+  attr_reader :content
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def valid?(new_content)
+    Product.all.each do |existing_product|
+      fail "DuplicateProductError: 'LEGO Iron Man vs. Ultron' already exists." if existing_product.content[:name] == new_content[:name]
+    end
+    true
+  end
+
+  def initialize(*args)
+    # Create a Hash from all args provided
+    @content = args.extract_options! if valid?(args.extract_options!)
+    @@all << self
+  end
 end
